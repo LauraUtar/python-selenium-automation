73c67d95c8491309991d6e506eb690261f500307
laurautarbayeva <laura.utarbayeva@retextion.com>
2023-02-20 16:36:19 -0600

    HW2

--- features/steps/amazonsigninlocators_hw2part2.py
+++ features/steps/amazonsigninlocators_hw2part2.py
@@ -12,18 +12,17 @@ driver.find_element(By.ID, 'ap_email')
 # Continue button
 driver.find_element(By. XPATH, '//input[@class="a-button-input"]')
 
-
 # Need help link
-
+driver.find_element(By.XPATH, '//span[@class="a-expander-prompt"]')
 
 # Forgot your password link
-
+driver.find_element(By.ID, 'auth-fpp-link-bottom')
 
 # Other issues with Sign-In link
-
+driver.find_element(By.ID, 'ap-other-signin-issues-link')
 
 # Create your Amazon account button
-
+driver.find_element(By.ID, 'createAccountSubmit')
 
 # *Conditions of use link
 
